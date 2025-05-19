65500f29b739282579e98443fdfb1eaaae24b31f
Android Studio <devandroid@ort>
2025-05-19 09:00:54 -0300

    agregue el fondo

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
+const imgExterna = {uri: 'https://www.prunellinmobiliaria.com.ar/wp-content/uploads/2012/06/Fondo-azul1.png'}
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ImageBackground 
+        source={imgExterna}
+        resizeMode="cover">
+      </ImageBackground>
     </View>
   );
 }
